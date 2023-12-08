4b5cdc5b057e1e3b62bc68acdc81a319c1dcb2b9
Tyluur <contact@tyluur.com>
2023-12-07 21:34:01 -0500

    feat: add part-2 of day-2 solution

--- src/main/kotlin/com/tyluur/day2/Day2.kt
+++ src/main/kotlin/com/tyluur/day2/Day2.kt
@@ -6,17 +6,31 @@ import java.io.File
 fun main() {
 	val filePath = "src/main/resources/day-2-input.txt"
 	val games = parseGames(filePath)
+
+	// Part 1
 	val feasibleGamesSum = games.filter { it.isFeasible(12, 13, 14) }.sumOf { it.id }
 	logger.info { "Sum of IDs of feasible games: $feasibleGamesSum" }
+
+	// Part 2
+	val totalPowerSum = games.sumOf { it.calculatePower() }
+	logger.info { "Sum of the power of minimum sets: $totalPowerSum" }
 }
 
 data class Game(val id: Int, val subsets: List<Map<String, Int>>) {
 	fun isFeasible(maxRed: Int, maxGreen: Int, maxBlue: Int): Boolean {
 		return subsets.all { subset ->
-			subset.getOrDefault("red", 0) <= maxRed && subset.getOrDefault(
-				"green", 0
-			) <= maxGreen && subset.getOrDefault("blue", 0) <= maxBlue
+			subset.getOrDefault("red", 0) <= maxRed &&
+					subset.getOrDefault("green", 0) <= maxGreen &&
+					subset.getOrDefault("blue", 0) <= maxBlue
 		}
+	}
+
+	// New function for Part 2
+	fun calculatePower(): Int {
+		val minRed = subsets.maxOfOrNull { it.getOrDefault("red", 0) } ?: 0
+		val minGreen = subsets.maxOfOrNull { it.getOrDefault("green", 0) } ?: 0
+		val minBlue = subsets.maxOfOrNull { it.getOrDefault("blue", 0) } ?: 0
+		return minRed * minGreen * minBlue
 	}
 }
 
