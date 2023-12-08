0c33e9a70ecf202dd80e1fb3227b560be31406a7
Tyluur <contact@tyluur.com>
2023-12-08 16:41:19 -0500

    feat: convert day 3 solution to use puzzle abstraction

--- src/main/kotlin/com/tyluur/day3/Day3.kt
+++ src/main/kotlin/com/tyluur/day3/Day3.kt
@@ -1,111 +1,97 @@
-import java.io.File
+package com.tyluur.day3
+
+import com.tyluur.Puzzle
 
 /**
- * A solver for Day 3's "Gear Ratios" puzzle.
- *
- * @author Tyluur <contact@tyluur.com>
- * @since December 7th, 2023
+ * Day 3 puzzle solution.
  */
-class Day3 {
+object Day3 : Puzzle<Array<IntArray>>(3) {
+	override fun parse(input: Sequence<String>): Array<IntArray> {
+		return input.map { line ->
+			line.toCharArray().map { char ->
+				when (char) {
+					'.' -> 0 // Use 0 to represent empty space
+					'*' -> -1 // Use -1 to represent symbols (adjust as needed)
+					else -> char.toString().toIntOrNull() ?: 0 // Use integer values for numbers
+				}
+			}.toIntArray()
+		}.toList().toTypedArray()
+	}
 
-	/**
-	 * Lazy-loaded list of strings representing the engine schematic.
-	 */
-	private val schematic: List<String> by lazy { File("src/main/resources/day-3-input.txt").readLines() }
+	override fun solvePart1(input: Array<IntArray>): Int {
+		var sum = 0
 
-	/**
-	 * Set of characters considered as symbols in the engine schematic.
-	 */
-	private val symbols = setOf('*', '#', '&', '@', '$', '+', '-', '%', '/', '=')
-
-	/**
-	 * Solves Part 1 of the puzzle by identifying and listing all part numbers.
-	 *
-	 * @return List of integers representing part numbers.
-	 */
-	fun solvePart1(): List<Int> {
-		return schematic.flatMapIndexed { y, line ->
-			line.mapIndexedNotNull { x, char ->
-				if (char.isDigit() && isAdjacentToSymbol(x, y)) char.toString().toInt() else null
+		// Iterate over the 2D array and calculate the sum of adjacent numbers
+		for (y in input.indices) {
+			for (x in input[y].indices) {
+				if (input[y][x] != -1 && isAdjacentToSymbol(input, x, y)) {
+					sum += input[y][x]
+				}
 			}
 		}
+
+		return sum
 	}
 
-	/**
-	 * Solves Part 2 of the puzzle by identifying and listing the gear ratios.
-	 *
-	 * @return List of integers representing gear ratios.
-	 */
-	fun solvePart2(): List<Int> {
-		return schematic.flatMapIndexed { y, line ->
-			line.mapIndexedNotNull { x, char ->
-				if (char == '*' && isGear(x, y)) calculateGearRatio(x, y) else null
+	override fun solvePart2(input: Array<IntArray>): Int {
+		var gearSum = 0
+
+		// Iterate over the 2D array and find and calculate the gear ratios
+		for (y in input.indices) {
+			for (x in input[y].indices) {
+				if (input[y][x] == -1 && isGear(input, x, y)) {
+					gearSum += calculateGearRatio(input, x, y)
+				}
 			}
 		}
+
+		return gearSum
 	}
 
-	/**
-	 * Checks if a given position in the schematic is adjacent to a symbol.
-	 *
-	 * @param x X-coordinate in the schematic.
-	 * @param y Y-coordinate in the schematic.
-	 * @return True if the position is adjacent to a symbol, false otherwise.
-	 */
-	private fun isAdjacentToSymbol(x: Int, y: Int): Boolean {
-		return getAdjacentPositions(x, y).any { (ax, ay) ->
-			schematic.getOrNull(ay)?.getOrNull(ax) in symbols
-		}
-	}
-
-	/**
-	 * Checks if a '*' at a given position represents a gear.
-	 *
-	 * @param x X-coordinate in the schematic.
-	 * @param y Y-coordinate in the schematic.
-	 * @return True if the '*' symbol represents a gear, false otherwise.
-	 */
-	private fun isGear(x: Int, y: Int): Boolean {
-		return getAdjacentPositions(x, y).count { (ax, ay) ->
-			schematic.getOrNull(ay)?.getOrNull(ax)?.isDigit() == true
-		} == 2
-	}
-
-	/**
-	 * Calculates the gear ratio for a gear at a given position.
-	 *
-	 * @param x X-coordinate of the gear in the schematic.
-	 * @param y Y-coordinate of the gear in the schematic.
-	 * @return Gear ratio as an integer.
-	 */
-	private fun calculateGearRatio(x: Int, y: Int): Int {
-		val partNumbers = getAdjacentPositions(x, y).mapNotNull { (ax, ay) ->
-			schematic.getOrNull(ay)?.getOrNull(ax)?.toString()?.toIntOrNull()
-		}
-		return partNumbers.reduce(Int::times)
-	}
-
-	/**
-	 * Gets positions adjacent to a given position in the schematic.
-	 *
-	 * @param x X-coordinate in the schematic.
-	 * @param y Y-coordinate in the schematic.
-	 * @return List of pairs representing adjacent positions.
-	 */
-	private fun getAdjacentPositions(x: Int, y: Int): List<Pair<Int, Int>> {
-		return listOf(
+	private fun isAdjacentToSymbol(input: Array<IntArray>, x: Int, y: Int): Boolean {
+		val adjacentPositions = listOf(
 			x - 1 to y, x + 1 to y, // left and right
 			x to y - 1, x to y + 1, // up and down
 			x - 1 to y - 1, x + 1 to y + 1, // diagonal
 			x - 1 to y + 1, x + 1 to y - 1
 		)
-	}
-}
 
-/**
- * Main function for solving Advent of Code 2023, Day 3 puzzles.
- */
-fun main() {
-	val solver = Day3() // Replace with the actual file path
-	println("List of part numbers (Part 1): ${solver.solvePart1().sum()}")
-	println("List of gear ratios (Part 2): ${solver.solvePart2().sum()}")
+		return adjacentPositions.any { (ax, ay) ->
+			ay >= 0 && ay < input.size && ax >= 0 && ax < input[ay].size && input[ay][ax] != 0
+		}
+	}
+
+	private fun isGear(input: Array<IntArray>, x: Int, y: Int): Boolean {
+		val adjacentPositions = listOf(
+			x - 1 to y, x + 1 to y, // left and right
+			x to y - 1, x to y + 1, // up and down
+			x - 1 to y - 1, x + 1 to y + 1, // diagonal
+			x - 1 to y + 1, x + 1 to y - 1
+		)
+
+		val adjacentNumbers = adjacentPositions.count { (ax, ay) ->
+			ay >= 0 && ay < input.size && ax >= 0 && ax < input[ay].size && input[ay][ax] > 0
+		}
+
+		return adjacentNumbers == 2
+	}
+
+	private fun calculateGearRatio(input: Array<IntArray>, x: Int, y: Int): Int {
+		val adjacentPositions = listOf(
+			x - 1 to y, x + 1 to y, // left and right
+			x to y - 1, x to y + 1, // up and down
+			x - 1 to y - 1, x + 1 to y + 1, // diagonal
+			x - 1 to y + 1, x + 1 to y - 1
+		)
+
+		val partNumbers = adjacentPositions.mapNotNull { (ax, ay) ->
+			if (ay >= 0 && ay < input.size && ax >= 0 && ax < input[ay].size) {
+				input[ay][ax]
+			} else {
+				0
+			}
+		}
+
+		return partNumbers.reduce(Int::times)
+	}
 }
