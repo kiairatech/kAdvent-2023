9b9fad5c3f667af0d4a994ce58c4f3f40a774341
Tyluur <contact@tyluur.com>
2023-12-08 19:49:15 -0500

    feat: improve solution for day 7

--- src/main/kotlin/com/tyluur/day7/Day7.kt
+++ src/main/kotlin/com/tyluur/day7/Day7.kt
@@ -2,167 +2,120 @@ package com.tyluur.day7
 
 import com.tyluur.Puzzle
 
-object Day7 : Puzzle<List<Hand>>(7) {
+/**
+ * Solution for Day 7 of the Advent of Code challenge.
+ * This class extends the Puzzle class and is specialized in handling poker hands.
+ *
+ * @author Tyluur
+ * @since December 8th, 2023
+ */
+object Day7 : Puzzle<List<Day7.Hand>>(7) {
+
+	/**
+	 * Parses the input into a list of Hand objects.
+	 * Each line of the input represents a hand of poker cards with a bid.
+	 *
+	 * @param input The input data as a sequence of strings.
+	 * @return A list of Hand objects.
+	 */
 	override fun parse(input: Sequence<String>): List<Hand> {
-		return input.map { Hand.parse(it) }.toList()
+		return input.map { line ->
+			val (cards, bid) = line.split(" ")
+			Hand(cards, bid.toInt(), false)
+		}.toList()
 	}
 
+	/**
+	 * Solves Part 1 of the Day 7 puzzle.
+	 * Calculates the total score of the hands without adjusting for part 2 rules.
+	 *
+	 * @param input The parsed input data as a list of Hand objects.
+	 * @return The total score as a Long.
+	 */
 	override fun solvePart1(input: List<Hand>): Long {
-		val sortedHands = input.sortedWith(HandComparator(wildcard = false))
-		return calculateWinnings(sortedHands)
+		return calculateTotalScore(input)
 	}
 
+	/**
+	 * Solves Part 2 of the Day 7 puzzle.
+	 * Adjusts the hands for part 2 rules and then calculates the total score.
+	 *
+	 * @param input The parsed input data as a list of Hand objects.
+	 * @return The total score as a Long.
+	 */
 	override fun solvePart2(input: List<Hand>): Long {
-		val sortedHands = input.sortedWith(HandComparator(wildcard = true))
-		return calculateWinnings(sortedHands)
+		val adjustedInput = input.map { it.copy(adjustForPart2 = true) }
+		return calculateTotalScore(adjustedInput)
 	}
 
-	private fun calculateWinnings(sortedHands: List<Hand>): Long {
-		var totalWinnings = 0L
-		val rankMap = mutableMapOf<Hand, Int>()
-
-		for ((index, hand) in sortedHands.withIndex()) {
-			val rank = rankMap[hand] ?: (index + 1)
-			rankMap[hand] = rank
-			totalWinnings += rank.toLong() * hand.bid
+	/**
+	 * Calculates the total score of a list of hands.
+	 * Sorts the hands based on their value and multiplies each hand's bid by its position.
+	 *
+	 * @param hands The list of Hand objects to calculate the score for.
+	 * @return The total score as a Long.
+	 */
+	private fun calculateTotalScore(hands: List<Hand>): Long {
+		val sortedHands = hands.sortedWith(HandComparator)
+		return sortedHands.foldIndexed(0L) { index, acc, hand ->
+			acc + (index + 1) * hand.bid
 		}
-
-		return totalWinnings
-	}
-}
-
-data class Hand(val cards: List<Card>, val bid: Int) {
-	val type = getType(cards, wildcard = false)
-	val wildcardType = getType(cards, wildcard = true)
-
-	init {
-		require(cards.size == 5)
 	}
 
-	companion object {
-		fun parse(s: String): Hand {
-			val (cards, bid) = s.split(' ', limit = 2)
-			return Hand(cards.map { Card.parse(it) }, bid.toInt())
+	/**
+	 * Data class representing a poker hand.
+	 * Contains information about the hand's cards, bid, and if it should adjust for Part 2 rules.
+	 *
+	 * @property cards String representation of the hand's cards.
+	 * @property bid The bid associated with the hand.
+	 * @property adjustForPart2 Flag to indicate if the hand should be adjusted for Part 2 rules.
+	 */
+	data class Hand(val cards: String, val bid: Int, val adjustForPart2: Boolean) {
+		val totalValue: String by lazy {
+			var cardsValue = cards.map {
+				if (adjustForPart2) weights2.indexOf(it).toString(16)
+				else weights.indexOf(it).toString(16)
+			}.joinToString("")
+
+			if (adjustForPart2 && cards.contains('J')) {
+				val maxTypeValue = replacements.map { r ->
+					getTypeValue(cards.replace('J', r))
+				}.maxOrNull() ?: throw IllegalStateException("Max type value not found")
+				return@lazy maxTypeValue.toString() + cardsValue
+			}
+			getTypeValue(cards).toString() + cardsValue
 		}
 
-		private fun getType(cards: List<Card>, wildcard: Boolean): HandType {
-			val bag = mutableMapOf<Card, Int>()
-			for (card in cards) {
-				bag[card] = bag.getOrDefault(card, 0) + 1
-			}
+		companion object {
+			private val typeValue = mapOf(
+				"5" to 7,
+				"41" to 6,
+				"32" to 5,
+				"311" to 4,
+				"221" to 3,
+				"2111" to 2,
+				"11111" to 1
+			)
+			private const val weights = "23456789TJQKA"
+			private const val weights2 = "J23456789TQKA"
+			private const val replacements = "23456789TQKA"
 
-			val jokers = if (wildcard) {
-				bag.remove(Card.JACK) ?: 0
-			} else {
-				0
-			}
-
-			return getBestType(bag.values.toList(), jokers)
-		}
-
-		private fun getBestType(counts: List<Int>, jokers: Int): HandType {
-			if (jokers == 0) {
-				return getType(counts)
-			}
-
-			// try joker as new card type
-			var best = getBestType(counts.plus(1), jokers - 1)
-
-			// try joker as every existing card type
-			for (i in counts.indices) {
-				val copy = counts.toMutableList()
-				copy[i]++
-
-				val type = getBestType(copy, jokers - 1)
-				if (type < best) {
-					best = type
-				}
-			}
-
-			return best
-		}
-
-		private fun getType(counts: List<Int>): HandType {
-			require(counts.sum() == 5)
-			require(counts.size in 1..5)
-
-			return when (counts.size) {
-				1 -> HandType.FIVE_OF_A_KIND
-				2 -> if (4 in counts) HandType.FOUR_OF_A_KIND else HandType.FULL_HOUSE
-				3 -> if (3 in counts) HandType.THREE_OF_A_KIND else HandType.TWO_PAIR
-				4 -> HandType.ONE_PAIR
-				else -> HandType.HIGH_CARD
+			fun getTypeValue(cards: String): Int {
+				val countMap = cards.groupingBy { it }.eachCount()
+				val sortedCounts = countMap.values.sortedDescending()
+				val key = sortedCounts.joinToString("")
+				return typeValue[key] ?: throw IllegalArgumentException("Invalid hand type")
 			}
 		}
 	}
-}
 
-class HandComparator(private val wildcard: Boolean) : Comparator<Hand> {
-	override fun compare(o1: Hand, o2: Hand): Int {
-		if (wildcard) {
-			val cmp = o1.wildcardType.compareTo(o2.wildcardType)
-			if (cmp != 0) {
-				return cmp
-			}
-		} else {
-			val cmp = o1.type.compareTo(o2.type)
-			if (cmp != 0) {
-				return cmp
-			}
-		}
-
-		for ((i, card) in o1.cards.withIndex()) {
-			val otherCard = o2.cards[i]
-
-			if (wildcard) {
-				if (card == Card.JACK && otherCard != Card.JACK) {
-					return 1
-				} else if (card != Card.JACK && otherCard == Card.JACK) {
-					return -1
-				}
-			}
-
-			val cmp = card.compareTo(otherCard)
-			if (cmp != 0) {
-				return cmp
-			}
-		}
-
-		return 0
-	}
-}
-
-enum class HandType {
-	FIVE_OF_A_KIND,
-	FOUR_OF_A_KIND,
-	FULL_HOUSE,
-	THREE_OF_A_KIND,
-	TWO_PAIR,
-	ONE_PAIR,
-	HIGH_CARD,
-}
-
-enum class Card {
-	ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO;
-
-	companion object {
-		fun parse(c: Char): Card {
-			return when (c) {
-				'A' -> ACE
-				'K' -> KING
-				'Q' -> QUEEN
-				'J' -> JACK
-				'T' -> TEN
-				'9' -> NINE
-				'8' -> EIGHT
-				'7' -> SEVEN
-				'6' -> SIX
-				'5' -> FIVE
-				'4' -> FOUR
-				'3' -> THREE
-				'2' -> TWO
-				else -> throw IllegalArgumentException()
-			}
+	/**
+	 * Comparator for Hand objects.
+	 * Compares two Hand objects based on their total value.
+	 */
+	object HandComparator : Comparator<Hand> {
+		override fun compare(o1: Hand, o2: Hand): Int {
+			return o1.totalValue.compareTo(o2.totalValue)
 		}
 	}
 }
