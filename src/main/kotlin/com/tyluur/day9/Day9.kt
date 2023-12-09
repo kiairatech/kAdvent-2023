e00b10d7266a90d6bf84142e58a8ad680643e377
Tyluur <contact@tyluur.com>
2023-12-09 00:17:28 -0500

    feat: add day 9 part 2 solution
    Signed-off-by: Tyluur <contact@tyluur.com>

--- src/main/kotlin/com/tyluur/day9/Day9.kt
+++ src/main/kotlin/com/tyluur/day9/Day9.kt
@@ -14,6 +14,10 @@ object Day9 : Puzzle<List<List<Int>>>(9) {
 		return input.sumOf { calculateNextValue(it) }
 	}
 
+	override fun solvePart2(input: List<List<Int>>): Int {
+		return input.sumOf { calculatePreviousValue(it) }
+	}
+
 	private fun calculateNextValue(history: List<Int>): Int {
 		val sequences = mutableListOf(history.toMutableList())
 
@@ -38,4 +42,27 @@ object Day9 : Puzzle<List<List<Int>>>(9) {
 		return sequences[0].last()
 	}
 
+	private fun calculatePreviousValue(history: List<Int>): Int {
+		val sequences = mutableListOf(history.toMutableList())
+
+		// Generate sequences of differences
+		while (true) {
+			val newSequence = (0 until sequences.last().size - 1).map { i ->
+				sequences.last()[i + 1] - sequences.last()[i]
+			}.toMutableList()
+
+			sequences.add(newSequence)
+
+			// Break if all differences are zero
+			if (newSequence.all { it == 0 }) break
+		}
+
+		// Extrapolate the previous value
+		for (i in sequences.size - 2 downTo 0) {
+			sequences[i].add(0, sequences[i].first() - sequences[i + 1].first())
+		}
+
+		// The previous value is the first element of the first sequence
+		return sequences[0].first()
+	}
 }
