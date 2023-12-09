ffcb86176a0312cd4c3c79c4914c874ea063541f
Tyluur <contact@tyluur.com>
2023-12-09 13:29:13 -0500

    feat: add benchmark generation
    Signed-off-by: Tyluur <contact@tyluur.com>

--- README.md
+++ README.md
@@ -2,16 +2,41 @@
 
 Welcome to `kadvent-2023`, a repository of my solutions to the Advent of Code 2023 puzzles, implemented in Kotlin. This project showcases my problem-solving skills and proficiency in Kotlin, aiming to provide efficient and readable solutions to various coding challenges presented during the Advent of Code 2023.
 
-## About Advent of Code
+### About Advent of Code
 
 Advent of Code is an annual set of Christmas-themed computer programming challenges. Each day from December 1st to December 25th, a new puzzle is released. The puzzles cover a broad range of programming concepts and provide a fun way to practice coding skills.
 
-## Project Structure
+### Project Structure
 
 This project is structured as follows:
 
 - Each day's puzzles are solved in separate Kotlin files, named according to the day (e.g., `Day1.kt`, `Day2.kt`, etc.).
 - Input data for each puzzle is stored in the `resources` directory.
+
+---
+
+## Benchmark Results
+
+| Day | Part | Solution        | Execution Time | Parsing Time (ms) |
+|-----|------|-----------------|----------------|-------------------|
+| 1   | 1    | 54953           | 5 ms           | 5.68              |
+| 1   | 2    | 53409           | 4 ms           | -                 |
+| 2   | 1    | 2204            | 0 ms           | 5.35              |
+| 2   | 2    | 71036           | 0 ms           | -                 |
+| 3   | 1    | 530495          | 22 ms          | 0.23              |
+| 3   | 2    | 80253814        | 176 ms         | -                 |
+| 4   | 1    | 53957           | 1 ms           | 8.82              |
+| 4   | 2    | 5744979         | 0 ms           | -                 |
+| 5   | 1    | 534869317       | 0 ms           | 9.45              |
+| 5   | 2    | 164020188       | 1 ms           | -                 |
+| 6   | 1    | 4403592         | 0 ms           | 0.70              |
+| 6   | 2    | 36              | 0 ms           | -                 |
+| 7   | 1    | 251287184       | 11 ms          | 5.18              |
+| 7   | 2    | 250757288       | 8 ms           | -                 |
+| 8   | 1    | 15989           | 2 ms           | 3.35              |
+| 8   | 2    | 13830919117339  | 15 ms          | -                 |
+| 9   | 1    | 1980437560      | 6 ms           | 3.22              |
+| 9   | 2    | 977             | 6 ms           | -                 |
 
 
 ---
--- src/main/kotlin/com/tyluur/Main.kt
+++ src/main/kotlin/com/tyluur/Main.kt
@@ -10,80 +10,116 @@ import com.tyluur.day6.Day6
 import com.tyluur.day7.Day7
 import com.tyluur.day8.Day8
 import com.tyluur.day9.Day9
+import kotlin.io.path.Path
+import kotlin.io.path.writeText
 import kotlin.time.Duration
 import kotlin.time.measureTimedValue
 
 /**
- * The main function for running Advent of Code puzzles.
- * It selects and runs puzzles, measures execution time, and prints the solutions.
- *
- * @param args Command line arguments, which can include the specific day number to run.
+ * The main class for the Advent of Code puzzle solutions.
+ * This class runs the puzzle solutions, measures their execution times,
+ * and generates a benchmark report.
  */
-fun main(args: Array<String>) {
-	// List of all puzzle objects for each day.
-	val puzzles = mutableListOf<Puzzle<*>>(
-		Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9
-	)
+object Main {
 
-	// If a specific day is provided in the command line arguments, filter to run only that day's puzzle.
-	val day = args.firstOrNull()?.toIntOrNull()
-	if (day != null) {
-		puzzles.removeIf { puzzle -> puzzle.number != day }
+	/** StringBuilder to accumulate benchmark results. */
+	private val benchmarkResults = StringBuilder()
+
+	/**
+	 * The main entry point of the application.
+	 *
+	 * @param args Command line arguments. If a specific day number is provided, only that day's puzzle is run.
+	 */
+	@JvmStatic
+	fun main(args: Array<String>) {
+		benchmarkResults.append("| Day | Part | Solution | Execution Time | Parsing Time |\n")
+		benchmarkResults.append("| --- | ---- | -------- | ------------- | ------------ |\n")
+
+		val puzzles = mutableListOf<Puzzle<*>>(
+			Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9
+		)
+
+		args.firstOrNull()?.toIntOrNull()?.let { day ->
+			puzzles.removeIf { puzzle -> puzzle.number != day }
+		}
+
+		for (puzzle in puzzles) {
+			solve(puzzle)
+		}
+
+		Path("benchmark.md").writeText(benchmarkResults.toString())
 	}
 
-	// Solve each puzzle in the list.
-	for (puzzle in puzzles) {
-		solve(puzzle)
+	/**
+	 * Solves a given puzzle and appends the results to the benchmark report.
+	 *
+	 * @param T The type of the parsed input data for the puzzle.
+	 * @param puzzle The puzzle to be solved.
+	 */
+	private fun <T> solve(puzzle: Puzzle<T>) {
+		// Measure time taken to parse the puzzle input.
+		val input = measureTimedValue {
+			puzzle.parse()
+		}
+
+		// Solve Part 1 of the puzzle and measure the time taken.
+		val solutionPart1 = measureTimedValue {
+			puzzle.solvePart1(input.value)
+		}
+		printSolution(puzzle.number, 1, solutionPart1.value, solutionPart1.duration, input.duration)
+
+		// Solve Part 2 of the puzzle, if implemented, and measure the time taken.
+		val solutionPart2 = measureTimedValue {
+			puzzle.solvePart2(input.value)
+		}
+		if (solutionPart2.value != null) {
+			printSolution(puzzle.number, 2, solutionPart2.value!!, solutionPart2.duration)
+		}
+
+		// Append benchmark results
+		appendBenchmarkResult(puzzle.number, 1, solutionPart1.value, solutionPart1.duration, input.duration)
+		if (solutionPart2.value != null) {
+			appendBenchmarkResult(puzzle.number, 2, solutionPart2.value!!, solutionPart2.duration)
+		}
 	}
+
+	/**
+	 * Appends the results of a puzzle solution to the benchmark report.
+	 *
+	 * @param day The day number of the puzzle.
+	 * @param part The part number of the puzzle solution.
+	 * @param solution The solution to the puzzle.
+	 * @param duration The duration of the puzzle solution execution.
+	 * @param parseDuration The duration of the puzzle input parsing, if applicable.
+	 */
+	private fun appendBenchmarkResult(
+		day: Int, part: Int, solution: Any, duration: Duration, parseDuration: Duration? = null
+	) {
+		val parseTime = parseDuration?.toString() ?: "-"
+		benchmarkResults.append("| $day | $part | $solution | ${duration.inWholeMilliseconds} ms | $parseTime |\n")
+	}
+
+	/**
+	 * Prints the solution of a puzzle along with the time taken for execution.
+	 *
+	 * @param day The day number of the puzzle.
+	 * @param part The part number of the puzzle solution.
+	 * @param solution The solution to the puzzle.
+	 * @param duration The duration of the puzzle solution execution.
+	 * @param parseDuration The duration of the puzzle input parsing, if applicable.
+	 */
+	private fun printSolution(day: Int, part: Int, solution: Any, duration: Duration, parseDuration: Duration? = null) {
+		val suffix = if (parseDuration != null) " + $parseDuration parsing" else ""
+		val value = solution.toString()
+
+		if (value.contains('\n')) {
+			logger.info { "Day $day Part $part: ($duration$suffix)" }
+			logger.info { value.trim() }
+		} else {
+			logger.info { "Day $day Part $part: $value ($duration$suffix)" }
+		}
+	}
+
+	/** Logger instance for logging execution details. */
+	private val logger = InlineLogger()
 }
-
-/**
- * Solves a given puzzle, measures the time taken for execution, and prints the results.
- *
- * @param T The type of the parsed input data for the puzzle.
- * @param puzzle The puzzle object to be solved.
- */
-private fun <T> solve(puzzle: Puzzle<T>) {
-	// Measure time taken to parse the puzzle input.
-	val input = measureTimedValue {
-		puzzle.parse()
-	}
-
-	// Solve Part 1 of the puzzle and measure the time taken.
-	val solutionPart1 = measureTimedValue {
-		puzzle.solvePart1(input.value)
-	}
-	printSolution(puzzle.number, 1, solutionPart1.value, solutionPart1.duration, input.duration)
-
-	// Solve Part 2 of the puzzle, if implemented, and measure the time taken.
-	val solutionPart2 = measureTimedValue {
-		puzzle.solvePart2(input.value)
-	}
-	if (solutionPart2.value != null) {
-		printSolution(puzzle.number, 2, solutionPart2.value!!, solutionPart2.duration)
-	}
-}
-
-/**
- * Prints the solution of a puzzle along with the time taken for execution.
- *
- * @param day The day number of the puzzle.
- * @param part The part number of the puzzle solution (1 or 2).
- * @param solution The solution to the puzzle.
- * @param duration The duration of the puzzle solution execution.
- * @param parseDuration The duration of the puzzle input parsing, if applicable.
- */
-private fun printSolution(day: Int, part: Int, solution: Any, duration: Duration, parseDuration: Duration? = null) {
-	val suffix = if (parseDuration != null) " + $parseDuration parsing" else ""
-	val value = solution.toString()
-
-	if (value.contains('\n')) {
-		logger.info { "Day $day Part $part: ($duration$suffix)" }
-		logger.info { value.trim() }
-	} else {
-		logger.info { "Day $day Part $part: $value ($duration$suffix)" }
-	}
-}
-
-/** The instance of the logger for this class */
-private val logger = InlineLogger()
