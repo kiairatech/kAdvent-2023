ca90ec766d183b1cf34fa7369c50be555ff9c94d
Tyluur <contact@tyluur.com>
2023-12-09 23:27:45 -0500

    docs: add comprehensive documentation to `Day9` class
    Enhanced the Day9 class in the Advent of Code solution package with detailed documentation for each method. Descriptions now clearly outline the purpose, parameters, return types, and processing logic. This update facilitates better understanding and maintainability of the code, especially for future collaborations or reviews.
    
    Signed-off-by: Tyluur <contact@tyluur.com>

--- src/main/kotlin/com/tyluur/day9/Day9.kt
+++ src/main/kotlin/com/tyluur/day9/Day9.kt
@@ -6,11 +6,11 @@ import com.tyluur.day9.Day9.number
 /**
  * Object representing the solution for Day 9 of the Advent of Code.
  *
- * It extends the Puzzle class and provides specific implementations for parsing and solving
- * the puzzle for Day 9, which involves extrapolating values from historical data.
+ * This class extends the Puzzle class and provides specific implementations for parsing and solving
+ * the puzzle for Day 9, which involves extrapolating values from historical data based on a series
+ * of integers.
  *
  * @property number The day number of the puzzle, which is 9.
- *
  * @author Tyluur <contact@tyluur.com>
  * @since December 9th, 2023
  */
@@ -18,7 +18,6 @@ object Day9 : Puzzle<List<List<Int>>>(9) {
 
 	/**
 	 * Parses the input data from a sequence of strings into a list of lists of integers.
-	 *
 	 * Each line of the input is split into a list of integers.
 	 *
 	 * @param input The input data as a sequence of strings.
@@ -32,11 +31,10 @@ object Day9 : Puzzle<List<List<Int>>>(9) {
 
 	/**
 	 * Solves Part 1 of the Day 9 puzzle.
-	 *
 	 * This involves summing the next extrapolated value for each series in the input.
 	 *
-	 * @param input The parsed input data.
-	 * @return The solution to Part 1 of the puzzle.
+	 * @param input The parsed input data, a list of lists of integers.
+	 * @return The solution to Part 1 of the puzzle as an integer.
 	 */
 	override fun solvePart1(input: List<List<Int>>): Int {
 		return input.sumOf { calculateValue(it, true) }
@@ -44,20 +42,42 @@ object Day9 : Puzzle<List<List<Int>>>(9) {
 
 	/**
 	 * Solves Part 2 of the Day 9 puzzle.
-	 *
 	 * This involves summing the previous extrapolated value for each series in the input.
 	 *
-	 * @param input The parsed input data.
-	 * @return The solution to Part 2 of the puzzle.
+	 * @param input The parsed input data, a list of lists of integers.
+	 * @return The solution to Part 2 of the puzzle as an integer.
 	 */
 	override fun solvePart2(input: List<List<Int>>): Int {
 		return input.sumOf { calculateValue(it, false) }
 	}
 
+	/**
+	 * Calculates the next or previous value in a series based on the difference method.
+	 *
+	 * This function uses a tail-recursive helper function to generate sequences of differences
+	 * until all differences are zero, then either extrapolates the next or previous value
+	 * based on the 'calculateNext' flag.
+	 *
+	 * @param history A list of integers representing the historical series.
+	 * @param calculateNext A boolean flag indicating the direction of extrapolation.
+	 *                      If true, calculates the next value, else calculates the previous value.
+	 * @return The extrapolated value as an integer.
+	 */
 	private fun calculateValue(history: List<Int>, calculateNext: Boolean): Int {
 		return calculate(history, mutableListOf(history.toMutableList()), calculateNext)
 	}
 
+	/**
+	 * Tail-recursive helper function for calculating the next or previous value in a series.
+	 *
+	 * This function iteratively generates sequences of differences and uses them to extrapolate
+	 * the next or previous value. The recursion terminates when all differences are zero.
+	 *
+	 * @param history The original series of historical values.
+	 * @param sequences A mutable list of mutable lists, each containing a sequence of differences.
+	 * @param calculateNext A boolean flag to determine the direction of extrapolation.
+	 * @return The extrapolated value as an integer.
+	 */
 	private tailrec fun calculate(
 		history: List<Int>,
 		sequences: MutableList<MutableList<Int>>,
