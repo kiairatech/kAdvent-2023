81213235cad52c0abd6bcc006b625aaa7d24e7e8
Tyluur <contact@tyluur.com>
2023-12-10 14:51:40 -0500

    feat: update `Day10` solution to follow better OOP practices
    Signed-off-by: Tyluur <contact@tyluur.com>

--- src/main/kotlin/com/tyluur/day10/Day10.kt
+++ src/main/kotlin/com/tyluur/day10/Day10.kt
@@ -2,79 +2,49 @@ package com.tyluur.day10
 
 import com.tyluur.Puzzle
 
-object Day10 : Puzzle<List<String>>(10) {
+/**
+ * Solves the "Day 10: Pipe Maze" puzzle from Advent of Code.
+ *
+ * This object inherits from the Puzzle class, providing implementations specific
+ * to solving both parts of the Day 10 puzzle. It uses the PipeMazeSolver class
+ * to analyze a maze of pipes and find solutions based on the maze's structure.
+ */
+object Day10 : Puzzle<PipeMazeSolver>(10) {
 
-	override fun parse(input: Sequence<String>): List<String> = input.toList()
-
-	override fun solvePart1(input: List<String>): Any {
-		val (start, direction) = findStart(input)
-		var point = start
-		var currentDirection = direction
-		var loopSize = 0
-
-		while (true) {
-			if (point == start && loopSize > 0) break
-			val result = traverse(point, currentDirection, input)
-			point = result.first
-			currentDirection = result.second
-			loopSize++
-		}
-
-		return loopSize / 2
+	/**
+	 * Parses the input for the Day 10 puzzle into a PipeMazeSolver.
+	 *
+	 * Converts the input sequence of strings into a list and then creates a
+	 * PipeMazeSolver instance from this list.
+	 *
+	 * @param input A sequence of strings representing the puzzle input.
+	 * @return An instance of PipeMazeSolver based on the provided input.
+	 */
+	override fun parse(input: Sequence<String>): PipeMazeSolver {
+		return PipeMazeSolver.fromInput(input.toList())
 	}
 
-	private fun findStart(input: List<String>): Pair<Pair<Int, Int>, Int> {
-		val y = input.indexOfFirst { line -> 'S' in line }
-		val x = input[y].indexOf('S')
-		val start = Pair(x, y)
-
-		val direction = when {
-			input.getOrNull(y - 1)?.get(x) in listOf('7', '|', 'F') -> 1
-			input[y].getOrNull(x - 1) in listOf('F', '-', 'L') -> 2
-			else -> 3 // Assuming the 'S' pipe connects to two directions.
-		}
-		return Pair(start, direction)
+	/**
+	 * Solves Part 1 of the Day 10 puzzle.
+	 *
+	 * Calculates the number of steps required to complete the loop in the pipe maze.
+	 *
+	 * @param input The parsed PipeMazeSolver instance representing the puzzle input.
+	 * @return The number of steps to complete the loop in the pipe maze.
+	 */
+	override fun solvePart1(input: PipeMazeSolver): Int {
+		return input.countSteps()
 	}
 
-	private fun traverse(point: Pair<Int, Int>, direction: Int, input: List<String>): Pair<Pair<Int, Int>, Int> {
-		val (x, y) = point
-		var newDirection = direction
-		val newPoint = when (direction) {
-			1 -> Pair(x, y - 1).also {
-				newDirection = when (input[y - 1][x]) {
-					'7' -> 2
-					'F' -> 3
-					else -> direction
-				}
-			}
-
-			2 -> Pair(x - 1, y).also {
-				newDirection = when (input[y][x - 1]) {
-					'L' -> 1
-					'F' -> 4
-					else -> direction
-				}
-			}
-
-			3 -> Pair(x + 1, y).also {
-				newDirection = when (input[y][x + 1]) {
-					'J' -> 1
-					'7' -> 4
-					else -> direction
-				}
-			}
-
-			4 -> Pair(x, y + 1).also {
-				newDirection = when (input[y + 1][x]) {
-					'L' -> 3
-					'J' -> 2
-					else -> direction
-				}
-			}
-
-			else -> point
-		}
-		return Pair(newPoint, newDirection)
+	/**
+	 * Solves Part 2 of the Day 10 puzzle.
+	 *
+	 * Determines the number of tiles that are enclosed within the main loop of the pipe maze.
+	 *
+	 * @param input The parsed PipeMazeSolver instance representing the puzzle input.
+	 * @return The number of tiles enclosed within the loop of the pipe maze.
+	 */
+	override fun solvePart2(input: PipeMazeSolver): Int {
+		return input.countEnclosed()
 	}
-
 }
