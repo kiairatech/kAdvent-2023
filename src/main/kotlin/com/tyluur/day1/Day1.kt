d35b8feb5f1e8f580522e8e8827c099f51166d68
Tyluur <contact@tyluur.com>
2023-12-10 23:12:49 -0500

    feat: update solution to day 1
    Signed-off-by: Tyluur <contact@tyluur.com>

--- src/main/kotlin/com/tyluur/day1/Day1.kt
+++ src/main/kotlin/com/tyluur/day1/Day1.kt
@@ -3,80 +3,77 @@ package com.tyluur.day1
 import com.tyluur.Puzzle
 
 /**
- * The Day 1 puzzle solution for Advent of Code 2023.
- * The puzzle involves calculating calibration values from a list of strings.
- * Part 1 calculates these directly, while Part 2 includes converting spelled-out numbers to digits.
+ * Day1 object represents the puzzle for Advent of Code Day 1.
+ * It extends the Puzzle class with a list of strings as input data.
  *
- * @author Tyluur
- * @since December 8th, 2023
+ * @constructor Creates a Day1 object with the puzzle number 1.
+ *
+ * @author Tyluur *
+ * @since December 10th, 2023
  */
 object Day1 : Puzzle<List<String>>(1) {
 
-	/**
-	 * Mapping of spelled-out numbers to their respective digits
-	 */
-	private val numberMapping = mapOf(
-		"one" to "1",
-		"two" to "2",
-		"three" to "3",
-		"four" to "4",
-		"five" to "5",
-		"six" to "6",
-		"seven" to "7",
-		"eight" to "8",
-		"nine" to "9"
+	// Mapping of spelled-out digits to their corresponding numerical values
+	private val digitsMap = mapOf(
+		"one"   to 1,
+		"two"   to 2,
+		"three" to 3,
+		"four"  to 4,
+		"five"  to 5,
+		"six"   to 6,
+		"seven" to 7,
+		"eight" to 8,
+		"nine"  to 9
 	)
 
 	/**
-	 * Parses the input sequence of strings into a list of strings.
+	 * Parses the input data for the Day1 puzzle.
 	 *
-	 * @param input The input data as a sequence of strings.
-	 * @return A list of strings representing the puzzle input.
+	 * @param input A sequence of strings representing the puzzle input.
+	 * @return A list of strings containing the parsed input data.
 	 */
 	override fun parse(input: Sequence<String>): List<String> = input.toList()
 
 	/**
-	 * Solves Part 1 of the Day 1 puzzle.
-	 * Sums up calibration values obtained from each line of input without replacing spelled-out numbers.
+	 * Solves Part 1 of the Day1 puzzle.
 	 *
-	 * @param input The parsed list of strings.
-	 * @return The sum of calibration values for Part 1.
+	 * @param input The parsed input data as a list of strings.
+	 * @return The solution to Part 1 of the puzzle.
 	 */
-	override fun solvePart1(input: List<String>): Any = input.sumOf { calculateCalibrationValue(it, false) }
-
-	/**
-	 * Solves Part 2 of the Day 1 puzzle.
-	 * Sums up calibration values obtained from each line of input, with spelled-out numbers replaced by digits.
-	 *
-	 * @param input The parsed list of strings.
-	 * @return The sum of calibration values for Part 2.
-	 */
-	override fun solvePart2(input: List<String>): Any = input.sumOf { calculateCalibrationValue(it, true) }
-
-	/**
-	 * Calculates the calibration value of a single line.
-	 * Optionally replaces spelled-out numbers with digits before calculation.
-	 *
-	 * @param line The line of text to process.
-	 * @param replaceWords Flag to indicate whether spelled-out numbers should be replaced.
-	 * @return The calculated calibration value of the line.
-	 */
-	private fun calculateCalibrationValue(line: String, replaceWords: Boolean): Int {
-		val processedLine = if (replaceWords) replaceSpelledOutNumbers(line) else line
-		val digits = processedLine.filter { it.isDigit() }
-		return when {
-			digits.length >= 2 -> "${digits.first()}${digits.last()}".toInt()
-			digits.length == 1 -> "${digits.first()}${digits.first()}".toInt()
-			else -> 0
+	override fun solvePart1(input: List<String>): Any {
+		return input.sumOf { line ->
+			val first = line.first(Char::isDigit)
+			val last = line.last(Char::isDigit)
+			"$first$last".toInt()
 		}
 	}
 
 	/**
-	 * Replaces all spelled-out numbers in a line with their corresponding digit representations.
+	 * Solves Part 2 of the Day1 puzzle.
 	 *
-	 * @param line The line of text to process.
-	 * @return The line with all spelled-out numbers replaced by digits.
+	 * @param input The parsed input data as a list of strings.
+	 * @return The solution to Part 2 of the puzzle.
 	 */
-	private fun replaceSpelledOutNumbers(line: String): String =
-		numberMapping.entries.fold(line) { acc, (word, digit) -> acc.replace(word, digit) }
+	override fun solvePart2(input: List<String>): Any {
+		return input.sumOf { line ->
+			val digits = mutableListOf<Int>()
+			var temp = line
+
+			while (temp.isNotEmpty()) {
+				if (temp.first().isDigit()) {
+					digits += temp.first().digitToInt()
+				} else {
+					digitsMap.forEach { (k, v) ->
+						if (temp.startsWith(k)) {
+							digits += v
+							return@forEach
+						}
+					}
+				}
+				temp = temp.drop(1)
+			}
+
+			digits.first() * 10 + digits.last()
+		}
+	}
 }
