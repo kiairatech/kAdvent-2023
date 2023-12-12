673c1d03cfbc2c68df2ff4a1acc5ccbaa6c83be4
Tyluur <contact@tyluur.com>
2023-12-12 18:58:02 -0500

    docs: add verbose documentation for SpringConditionRecords class
    Added detailed documentation for the SpringConditionRecords class in the package ca.kiaira.advent2023.day12. This includes comprehensive descriptions of the class purpose, its properties (springConditions and damagedGroups), example usage, and potential real-world applications. This update aims to enhance understanding and usability of the SpringConditionRecords class for future maintenance and development efforts.

--- src/main/kotlin/ca/kiaira/advent2023/day12/SpringConditionRecords.kt
+++ src/main/kotlin/ca/kiaira/advent2023/day12/SpringConditionRecords.kt
@@ -1,10 +1,34 @@
 package ca.kiaira.advent2023.day12
 
 /**
- * Encapsulates the puzzle input consisting of spring conditions and damaged groups.
+ * Represents a collection of spring condition records and damaged group information.
+ * This class is designed to encapsulate and manage data pertaining to a set of springs,
+ * specifically their operational conditions and any groupings of damage that may exist.
  *
- * @property springConditions A list of [SpringCondition] representing each row's spring condition.
- * @property damagedGroups A list of [DamagedGroup] representing groups of damaged springs.
+ * @property springConditions A list of [SpringCondition] objects. Each [SpringCondition]
+ *                            represents the condition of a spring in a specific row. This list
+ *                            serves as a comprehensive record of the operational status of each
+ *                            spring, where each spring's condition is detailed individually.
+ *
+ * @property damagedGroups A list of [DamagedGroup] objects. Each [DamagedGroup] represents a
+ *                         group of springs that have been collectively identified as damaged.
+ *                         This list enables the tracking and management of springs that are
+ *                         part of the same damage event or category, thus facilitating targeted
+ *                         repair or inspection efforts.
+ *
+ * The combination of these two properties provides a complete overview of the spring conditions,
+ * enabling efficient monitoring and maintenance of the spring system.
+ *
+ * Example Usage:
+ * ```
+ * val springConditionList = listOf(SpringCondition(...), SpringCondition(...))
+ * val damagedGroupList = listOf(DamagedGroup(...), DamagedGroup(...))
+ * val springConditionRecords = SpringConditionRecords(springConditionList, damagedGroupList)
+ * ```
+ *
+ * This class can be utilized in scenarios such as predictive maintenance systems, where
+ * understanding both individual spring conditions and grouped damage patterns is crucial for
+ * optimal operation and maintenance planning.
  */
 class SpringConditionRecords(
 	val springConditions: List<SpringCondition>,
